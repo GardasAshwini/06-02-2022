2eec8e13f6f736cdd8824a4ba54193ff4e5306d7
aman gardas <aman gardas@DESKTOP-AQAT1N6>
2022-02-07 11:12:33 +0530

    com.javaVariables added in the project 06-02-2022

---
+++ 06-02-2022/src/com/javaVaribles/example1.java
@@ -0,0 +1,35 @@
+package com.javaVaribles;
+
+public class example1 {
+
+	public static void main(String[] args) {
+		// Write a java program to print student information using java variable and literals
+		
+		
+		// Variable declaration
+		int studentId;
+		String studentName;
+		float studentMarks;
+		String studentAddress;
+		
+		// Variable initilization
+		studentId = 3020;
+		studentName = "Ashwini";
+		studentMarks = 58.45f;
+		studentAddress = "pune";
+		
+		System.out.print("sudent Id: ");
+		System.out.println(studentId);
+		
+		System.out.print("sudent Name: ");
+		System.out.println(studentName);
+		
+		System.out.print("sudent Marks: ");
+		System.out.println(studentMarks);
+		
+		System.out.print("sudent Address: ");
+		System.out.println(studentAddress);
+
+	}
+
+}
